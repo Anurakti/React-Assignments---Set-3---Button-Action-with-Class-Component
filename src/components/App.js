b37f0f5de45a6c0845dc3b9da8700b55386db957
Anurakti <anuraktiarora92@gmail.com>
2021-08-07 14:04:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,26 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+	        this.state = {
+      			paragraph: ''
+    }
 	};
+		displayParagraph = () => {
+		this.setState({paragraph: 'Hello, I\'ve learnt to use the full-stack evaluation tool. This makes me so happy'
+  })
+}
 
     render() {
     	return(
     		<div id="main">
+          {this.state.paragraph ==='Hello, I\'ve learnt to use the full-stack evaluation tool. This makes me so happy' && <p id='para'>{this.state.paragraph}</p>}
 				{ /* Do not remove this main div!! */ }
+			<button id='click' onClick={this.displayParagraph}>CLICK</button>
     		</div>
     	);
     }
 }
+
 
 
 export default App;
